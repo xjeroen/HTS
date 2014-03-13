a7008a0606a5c3fcd49da0de55cf3164039dd2f4
xjeroen <jeroen.c.bongers@gmail.com>
2014-03-13 21:36:09 +0100

    Removing git test

---
+++ HTS11/Decoder.py
@@ -0,0 +1,38 @@
+__author__ = 'Jeroen'
+
+import re
+import urllib.parse
+import urllib.request
+import http.cookiejar
+
+
+username = "username"
+password = "password"
+
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+login_data = urllib.parse.urlencode({'username': username, 'password': password})
+binary_data = login_data.encode('ascii')
+opener.open("https://www.hackthissite.org/user/login", binary_data)
+opener.addheaders.append(('Cookie', 'PHPSESSID=b8i37t5i8r9ppoddc1kbqqi180'))
+resp = opener.open("https://www.hackthissite.org/missions/prog/11/")
+
+URLString = resp.read().decode("utf-8")
+encodedString = re.search(r'Generated String: (.*?<)', URLString, re.I | re.M)
+encodedString = encodedString.group(1)[:-1]
+shift = re.search(r'Shift: (.*?<)', URLString, re.I | re.M)
+shift = int(shift.group(1)[:-1])
+
+stringLst = re.sub(r'[^0-9]', "$", encodedString)
+stringLst = stringLst.split("$")[:-1]
+value = ""
+for x in stringLst:
+    value += chr(int(x)-shift)
+
+print(value)
+query_args = {'solution': value}
+data = urllib.parse.urlencode(query_args)
+binData = data.encode('ascii')
+opener.addheaders.append(('Referer', 'http://www.hackthissite.org/'))
+try11 = opener.open("https://www.hackthissite.org/missions/prog/11/index.php", binData)
+print(try11.read())
