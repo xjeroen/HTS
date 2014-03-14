cefaec1bc7d32a9bb85c454fa35adafcf9fb9572
xjeroen <jeroen.c.bongers@gmail.com>
2014-03-14 23:21:08 +0100

    Bot now has line parser written in a function for cleaner code. Also added the !Author message

--- IRC.py
+++ IRC.py
@@ -17,6 +17,23 @@ irc.send(bytes('USER xjBot xjBot xjBot : xj IRC\r\n', 'utf-8'))
 irc.send(bytes('JOIN ' + BOT_IRC_CHANNEL+'\r\n', 'utf-8'))
 
 
+def messagechecker(msgLine):
+    completeLine = str(msgLine[1:]).split(':', 1)
+    info = completeLine[0].split()
+    message = completeLine[1].split("\\")[0]
+    sender = info[0][2:].split("!", 1)[0]
+    print("Complete Line-->"+str(completeLine))
+    print("Info-->"+str(info))
+    print("Message-->"+str(message))
+    print("Sender-->"+str(sender)+"\n")
+
+    if message.lower() == "hi":
+        irc.send(bytes('PRIVMSG '+BOT_IRC_CHANNEL+' :Hi there, '+str(sender)+'!\r\n', 'utf-8'))
+    if message.lower()[0] == '!' and sender == BOT_OWNER:
+        messageCommand = message[1:].split()
+        if messageCommand[0] == "Author":
+            irc.send(bytes('PRIVMSG '+BOT_IRC_CHANNEL+' :I was created by '+BOT_OWNER+'\r\n', 'utf-8'))
+
 while 1:
     line = irc.recv(4096)
     print(line)
@@ -26,15 +43,6 @@ while 1:
             print('PONG')
             irc.send(bytes('PONG ' + str(line[1])+'\r\n', 'utf-8'))
     elif line.find(bytes('PRIVMSG', 'utf-8')) != -1:
-        completeLine = str(line[1:]).split(':', 1)
-        info = completeLine[0].split()
-        message = completeLine[1].split("\\")[0]
-        sender = info[0][2:].split("!", 1)[0]
-        print("Complete Line-->"+str(completeLine))
-        print("Info-->"+str(info))
-        print("Message-->"+str(message))
-        print("Sender-->"+str(sender)+"\n")
-        if message.lower() == "hi":
-            irc.send(bytes('PRIVMSG '+BOT_IRC_CHANNEL+' :Hi there, '+str(sender)+'!\r\n', 'utf-8'))
+        messagechecker(line)
 
 
