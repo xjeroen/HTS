fe7689d88bbab6337c00586ba652d4d3c3c49469
xjeroen <jeroen.c.bongers@gmail.com>
2014-03-14 19:34:31 +0100

    Bot prevents timout with PONG now.

--- IRC.py
+++ IRC.py
@@ -14,20 +14,15 @@ irc.connect((BOT_IRC_SERVER, BOT_IRC_PORT))
 irc.recv(4096)
 irc.send(bytes('NICK '+BOT_NICKNAME+'\r\n', 'utf-8'))
 irc.send(bytes('USER xjBot xjBot xjBot : xj IRC\r\n', 'utf-8'))
-
+irc.send(bytes('JOIN ' + BOT_IRC_CHANNEL+'\r\n', 'utf-8'))
 
 
 while 1:
     line = irc.recv(4096)
     print(line)
-    if line.find(bytes('Welcome to the GeekShed IRC Network', 'utf-8')) != -1:
-        irc.send(bytes('JOIN ' + BOT_IRC_CHANNEL+'\r\n', 'utf-8'))
     if line.find(bytes('PING', 'utf-8')) != -1:
-        #parsemsg(line)
-        line = line.rstrip()
-        line = line.split()
-        print("Line 0:"+str(line[0]))
-        if line[0] == "b'PING'":
-            print('Succes')
-            irc.send(bytes('PONG ' + line[1]+'\r\n'))
+        line = line.rstrip().split()
+        if line[0] == bytes("PING", 'utf-8'):
+            print('PONG')
+            irc.send(bytes('PONG ' + str(line[1])+'\r\n', 'utf-8'))
 
