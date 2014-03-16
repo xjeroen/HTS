9ec2ab1a88bdb304a318ddddb56c48b122b296fb
xjeroen <jeroen.c.bongers@gmail.com>
2014-03-16 17:40:21 +0100

    -Bot replies to CTCP version request. -Bot responds to "moo's" !perm8-attack and joins #takeoverz -Bot kick's moo in response to !perm8-attack

--- HTS08/IRC.py
+++ HTS08/IRC.py
@@ -4,7 +4,7 @@ import socket
 import hashlib
 from time import sleep
 
-BOT_IRC_SERVER = "hub.irc.hackthissite.org"
+BOT_IRC_SERVER = "irc-hub.hackthissite.org"
 BOT_IRC_CHANNEL = "#perm8"
 BOT_IRC_PORT = 6667
 BOT_NICKNAME = "xjBot"
@@ -31,17 +31,23 @@ def messagechecker(msgLine):
 
     if message.lower() == "hi":
         irc.send(bytes('PRIVMSG ' + BOT_IRC_CHANNEL + ' :Hi there, ' + str(sender) + '!\r\n', 'utf-8'))
-    if message.lower()[0] == '!' and sender == BOT_OWNER:
+
+    if message[0] == '!' and sender == BOT_OWNER:
         messageCommand = message[1:].split()
         if messageCommand[0] == "Author":
             irc.send(bytes('PRIVMSG ' + BOT_IRC_CHANNEL + ' :I was created by ' + BOT_OWNER + '\r\n', 'utf-8'))
-    if message.lower()[0:4] == '!md5':  # checks if the server sends the message
-        messageString = message.split('!md5 ')[1:]
-        print("messageString-->"+messageString[0])
-        hash = hashlib.md5(messageString[0].encode('utf-8')).hexdigest()  # encrypts the message with md5
-        print("hash-->"+hash)
-        irc.send(bytes('NOTICE moo !perm8-result '+hash+'\r\n', 'utf-8'))  # sends the encrypted message back to the server
 
+    if sender == 'moo':
+        if message[0:4] == '!md5':  # checks if the server sends the message
+            messageString = message.split('!md5 ')[1:] #get the message without the info
+            msghash = hashlib.md5(messageString[0].encode('utf-8')).hexdigest()  # encrypts the message with md5
+            irc.send(bytes('NOTICE moo !perm8-result '+msghash+'\r\n', 'utf-8'))  # sends the encrypted message back to the server
+        if message.find('version'):  # if moo sends VERSION CTCP request
+            irc.send(bytes('NOTICE moo :\001VERSION xjBot:1.0:Windows\001\r\n', 'utf-8'))  # return VERSION
+        if message.find('!perm8-attack'):
+            irc.send(bytes('JOIN #takeoverz\r\n', 'utf-8'))
+            sleep(0.5)
+            irc.send(bytes('KICK #takeoverz moo\r\n', 'utf-8'))
 
 
 def sendmessage(rcv, msg):
@@ -49,7 +55,7 @@ def sendmessage(rcv, msg):
     irc.send(bytes('PRIVMSG ' + rcv + ' :' + msg + '\r\n', 'utf-8'))
 
 
-irc = socket.socket()  # socket.AF_INET, socket.SOCK_STREAM
+irc = socket.socket()
 irc.connect((BOT_IRC_SERVER, BOT_IRC_PORT))
 irc.recv(4096)
 irc.send(bytes('NICK ' + BOT_NICKNAME + '\r\n', 'utf-8'))
@@ -58,7 +64,7 @@ irc.send(bytes('USER xjBot xjBot xjBot : xj IRC\r\n', 'utf-8'))
 pingChecker(irc.recv(4096))
 #irc.send(bytes('JOIN ' + BOT_IRC_CHANNEL + '\r\n', 'utf-8'))
 sendmessage('NickServ', 'IDENTIFY '+BOT_NICKNAME+' '+BOT_PASSWORD)
-sleep(1)
+irc.send(bytes('ns set autoop on\r\n', 'utf-8'))
 irc.send(bytes('NOTICE moo !perm8\r\n', 'utf-8'))
 
 while 1:
